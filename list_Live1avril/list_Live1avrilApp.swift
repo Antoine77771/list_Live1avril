7be4dd3fb4049e6213140474e75ce8f6d0ab68ff
Antoine <antoineguerregenton@gmail.com>
2022-04-02 10:44:46 +0200

    Initial Commit

---
+++ list_Live1avril/list_Live1avrilApp.swift
@@ -0,0 +1,17 @@
+//
+//  list_Live1avrilApp.swift
+//  list_Live1avril
+//
+//  Created by Antoine on 02/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct list_Live1avrilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
